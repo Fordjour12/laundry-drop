ee3c539254758520c3b817fdfc733057a280b971
Bobie Fordjour McCamble Kofi <fordjourbobiem@gmail.com>
2023-08-28 12:55:44 +0000

    feat(user): code changes

--- src/controller/users.controller.ts
+++ src/controller/users.controller.ts
@@ -1,7 +1,11 @@
 import { NextFunction, Request, Response } from 'express'
+import { FindUserByEmail } from '../services/user.service.js'
+i
 
 const createUserController = async (
-	request: Request,
+	request: Request<{}, {},>,
 	response: Response,
 	next: NextFunction
-) => {}
+) => {
+	const user = FindUserByEmail()
+}
---
+++ src/schema/user.schema.ts
@@ -0,0 +1,30 @@
+import { z } from 'zod'
+
+enum Accountvalidataion {
+	NOT_VALID,
+	VALID,
+}
+
+const createUserSchema = z.object({
+	username: z.coerce
+		.string({ required_error: 'username is required' })
+		.min(3),
+	email: z.coerce
+		.string({
+			required_error: 'Email Address is required ',
+		})
+		.email(),
+	password: z.coerce
+		.string({ required_error: 'password is required' })
+		.min(8, 'must be at least 8 characters')
+		.max(32, 'must be at most 32  characters'),
+	phoneNumber: z.coerce
+		.number({ required_error: 'Phone number is required' })
+		.min(10, 'must be exact 10 numbers ')
+		.max(10, 'must be exact 10 numbers'),
+	account_state: z.optional(z.nativeEnum(Accountvalidataion)),
+})
+
+const 
+
+export { createUserSchema }
