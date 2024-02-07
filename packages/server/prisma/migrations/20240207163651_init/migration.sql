8c5cb3f0ec66e1e74f92bce95977e5a6f857d4a8
Bobie Fordjour McCamble Kofi <fordjourbobiem@gmail.com>
2024-02-07 18:59:59 +0000

    Add initial database schema and migration SQL scripts

--- packages/server/prisma/migrations/20240207163651_init/migration.sql
+++ packages/server/prisma/migrations/20240207163651_init/migration.sql
@@ -11,6 +11,28 @@ CREATE TABLE "company" (
 );
 
 -- CreateTable
+CREATE TABLE "cmpRefreshToken" (
+    "id" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "companyId" INTEGER NOT NULL,
+
+    CONSTRAINT "cmpRefreshToken_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "userRefreshToken" (
+    "id" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "userRefreshToken_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "user" (
     "id" TEXT NOT NULL,
     "username" TEXT NOT NULL,
@@ -50,6 +72,12 @@ CREATE UNIQUE INDEX "company_email_key" ON "company"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
+
+-- AddForeignKey
+ALTER TABLE "cmpRefreshToken" ADD CONSTRAINT "cmpRefreshToken_companyId_fkey" FOREIGN KEY ("companyId") REFERENCES "company"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "userRefreshToken" ADD CONSTRAINT "userRefreshToken_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "user" ADD CONSTRAINT "user_geoJsonId_fkey" FOREIGN KEY ("geoJsonId") REFERENCES "GeoJson"("id") ON DELETE SET NULL ON UPDATE CASCADE;
