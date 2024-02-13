d34297427b89103f7b812883fbea8e4b8f3087bd
Bobie Fordjour McCamble Kofi <fordjourbobiem@gmail.com>
2024-02-13 23:23:00 +0000

    Refactor signup page server load and register actions

--- packages/client/src/routes/signup/+page.server.ts
+++ packages/client/src/routes/signup/+page.server.ts
@@ -1,59 +1,54 @@
 import { fail, redirect, type Actions } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
 
-export const load = async ({ cookies }) => {
-    const sessionId = cookies.get('session_id');
+export const load: PageServerLoad = async ({ cookies }) => {
+	const sessionId = cookies.get('session_id');
 
-    if (sessionId) {
-        return redirect(301, '/dashboard');
-    }
+	if (sessionId) {
+		return redirect(301, '/dashboard');
+	}
 };
 
 export const actions = {
-    register: async ({ request, fetch, cookies }) => {
-        const data = await request.formData();
+	register: async ({ request, fetch, cookies }) => {
+		const data = await request.formData();
 
-        const email = data.get('email');
-        const password = data.get('password');
-        const name = data.get('name');
+		const email = data.get('email');
+		const password = data.get('password');
+		const name = data.get('name');
 
-        console.log(email, password, name);
-        if (
-            typeof email !== 'string' ||
-            typeof password !== 'string' ||
-            typeof name !== 'string' ||
-            !email ||
-            !password ||
-            !name
-        ) {
-            return fail(400, { invalid: true, message: 'Invalid email or password' });
-        }
+		console.log(email, password, name);
+		if (
+			typeof email !== 'string' ||
+			typeof password !== 'string' ||
+			typeof name !== 'string' ||
+			!email ||
+			!password ||
+			!name
+		) {
+			return fail(400, { invalid: true, message: 'Invalid email or password' });
+		}
 
-        const response = await fetch('http://127.0.0.1:5000/auth/company/signup', {
-            method: 'POST',
-            mode: 'cors',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ email, password, name })
-        });
+		const response = await fetch('http://127.0.0.1:5000/auth/company/signup', {
+			method: 'POST',
+			mode: 'cors',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ email, password, name })
+		});
 
-        if (response.ok) {
-            const sessionId = response.headers.get('Authorization');
-            console.log(sessionId);
-            cookies.set('session_id', sessionId?.split('Bearer ')[1] ?? '', {
-                path: '/'
-            });
+		if (response.ok) {
+			const sessionId = response.headers.get('Authorization');
+			console.log(sessionId);
+			cookies.set('session_id', sessionId?.split('Bearer ')[1] ?? '', {
+				path: '/'
+			});
 
-            throw redirect(301, '/dashboard');
-        } else {
-            const data = await response.json();
-            return fail(response.status, data);
-        }
-
-        // const resData = await response.json();
-
-        // if (response.ok) {
-        // 	return { success: true, data: resData };
-        // }
-    }
+			throw redirect(301, '/dashboard');
+		} else {
+			const data = await response.json();
+			return fail(response.status, data);
+		}
+	}
 } satisfies Actions;
