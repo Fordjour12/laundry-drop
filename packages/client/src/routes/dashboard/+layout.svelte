6ec65784e85d4b21d3967a80ed8942f93dd25f9d
Bobie Fordjour McCamble Kofi <fordjourbobiem@gmail.com>
2024-02-14 21:07:46 +0000

    Add dashboard layout server file and update dashboard layout component

--- packages/client/src/routes/dashboard/+layout.svelte
+++ packages/client/src/routes/dashboard/+layout.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
 	import Menu from '$lib/components/sv-component/menu.svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+	$: console.log(data.company?.name);
 </script>
 
 <main class="flex min-h-screen w-full">
-	<Menu />
+	<Menu cmpName={String(data.company?.name)} />
 	<slot />
 </main>
