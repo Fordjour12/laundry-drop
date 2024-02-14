a28556493a89deb33d3e876324ed2fb997d884e8
Bobie Fordjour McCamble Kofi <fordjourbobiem@gmail.com>
2024-02-14 21:04:35 +0000

    Add Locals interface with session and company properties

--- packages/client/src/app.d.ts
+++ packages/client/src/app.d.ts
@@ -1,9 +1,18 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+
+type Company = {
+	name: string;
+	email: string;
+};
+
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			session: string | null;
+			company: Company | null;
+		}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
