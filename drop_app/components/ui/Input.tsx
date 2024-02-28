ed451e2c32c83fc2919eafff48fc8897f19e011b
Bobie Fordjour McCamble Kofi <fordjourbobiem@gmail.com>
2024-02-28 16:27:18 +0000

    Add Input and Label  custom components

---
+++ drop_app/components/ui/Input.tsx
@@ -0,0 +1,19 @@
+// import React from "react";
+import { TextInput, TextInputProps } from "react-native";
+
+export type InputProps = TextInputProps & {
+  className?: string;
+};
+
+export default function Input(props: InputProps) {
+  const defaultStyle =
+    "bg-transparent rounded-lg py-4 px-4 border-2 border-gray-500 text-black w-full placeholder:text-black";
+  const { style, className, ...otherProps } = props;
+  return (
+    <TextInput
+      {...otherProps}
+      className={`${defaultStyle} ${className}`}
+      style={[style]}
+    />
+  );
+}
