dff9e35f1a5b13b6e7ac9aea5a89e3bf9a2183b9
Bobie Fordjour McCamble Kofi <fordjourbobiem@gmail.com>
2024-02-28 16:35:12 +0000

    Refactor tab layout and add login functionality

--- drop_app/app/(tabs)/two.tsx
+++ drop_app/app/(tabs)/two.tsx
@@ -1,5 +1,17 @@
-import { Text } from "react-native";
+import { Text, View, Pressable, TouchableOpacity } from "react-native";
 
 export default function TabTwoScreen() {
-  return <Text className="text-blue-600">Hello from tab 2</Text>;
+    return (
+        <View>
+            <Text className="text-teal-600">Hello from tab 2</Text>
+            <Pressable className="w-full py-4 text-red-600 bg-red-600" onPress={() => console.log("hello here")}>
+                <Text className="text-center text-white py-4 my-4">Login</Text>
+            </Pressable>
+
+            <TouchableOpacity className="border-white border w-full py-4 px-6 rounded-lg bg-yellow-500">
+                <Text className="text-teal-600">Hellow </Text>
+            </TouchableOpacity>
+        </View>
+    )
+
 }
