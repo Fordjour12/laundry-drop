063a9dd3a5a41acc1b546b84b7bb5dd4967fdbce
Bobie Fordjour McCamble Kofi <fordjourbobiem@gmail.com>
2024-03-01 02:16:55 +0000

    Add sign out functionality and redirect to sign-in page

--- drop_app/app/(app)/(tabs)/index.tsx
+++ drop_app/app/(app)/(tabs)/index.tsx
@@ -1,4 +1,5 @@
 import { useSession } from "@/context/auth/ctx";
+import { router } from "expo-router";
 import React from "react";
 import { Pressable, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -9,7 +10,12 @@ export default function index() {
     <SafeAreaView>
       <View>
         <Text>index</Text>
-        <Pressable onPress={() => signOut}>
+        <Pressable
+          onPress={() => {
+            signOut();
+            router.replace("/sign-in");
+          }}
+        >
           <Text>Sign Out</Text>
         </Pressable>
       </View>
