2d997c6bcf9da780edcdc79356ed0d24d5771312
Bobie Fordjour McCamble Kofi <fordjourbobiem@gmail.com>
2024-03-03 02:51:17 +0000

    Refactor authentication logic in TabLayout component

--- drop_app/app/(app)/(tabs)/index.tsx
+++ drop_app/app/(app)/(tabs)/index.tsx
@@ -1,23 +1,12 @@
-import { useSession } from "@/context/auth/ctx";
-import { router } from "expo-router";
 import React from "react";
-import { Pressable, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function index() {
-  const { signOut } = useSession();
   return (
     <SafeAreaView>
       <View>
         <Text>index</Text>
-        <Pressable
-          onPress={() => {
-            signOut();
-            router.replace("/sign-in");
-          }}
-        >
-          <Text>Sign Out</Text>
-        </Pressable>
       </View>
     </SafeAreaView>
   );
