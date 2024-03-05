c01c23c00d41c704a3000ddccee6a52890036828
Bobie Fordjour McCamble Kofi <fordjourbobiem@gmail.com>
2024-03-05 01:44:12 +0000

    Refactor index.tsx to improve code readability and add missing semicolon

--- drop_app/app/(app)/(tabs)/index.tsx
+++ drop_app/app/(app)/(tabs)/index.tsx
@@ -1,23 +1,21 @@
 import { useAuth } from "@/context/auth/authContext";
-import React from "react";
 import { Pressable, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function index() {
-    const { onLogout } = useAuth()
+  const { onLogout } = useAuth();
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>index</Text>
 
-
-
-    return (
-
-        <SafeAreaView>
-            <View>
-                <Text>index</Text>
-
-                <Pressable onPress={() => onLogout!()} className="bg-red-600 p-4 w-full " >
-                    <Text className="text-white font-bold text-center" >Sign out </Text>
-                </Pressable>
-            </View>
-        </SafeAreaView>
-    );
+        <Pressable
+          onPress={() => onLogout!()}
+          className="bg-red-600 p-4 w-full "
+        >
+          <Text className="text-white font-bold text-center">Sign out </Text>
+        </Pressable>
+      </View>
+    </SafeAreaView>
+  );
 }
