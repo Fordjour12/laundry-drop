ab9674d482e08a61aa0a830f0d8f345d933b70dc
Bobie Fordjour McCamble Kofi <fordjourbobiem@gmail.com>
2024-03-05 02:29:17 +0000

    Refactor authContext.tsx

--- drop_app/context/auth/authContext.tsx
+++ drop_app/context/auth/authContext.tsx
@@ -4,132 +4,132 @@ import * as SecureStore from "expo-secure-store";
 import { createContext, useContext, useEffect, useState } from "react";
 
 type AuthContextType = {
-    authState?: { token: string | null; isAuthenticated: boolean | null };
-    onRegister?: (
-        email: string,
-        password: string,
-        username: string
-    ) => Promise<any>;
-    onLogin?: (email: string, password: string) => Promise<any>;
-    onLogout?: () => Promise<any>;
+  authState?: { token: string | null; isAuthenticated: boolean | null };
+  onRegister?: (
+    email: string,
+    password: string,
+    username: string
+  ) => Promise<any>;
+  onLogin?: (email: string, password: string) => Promise<any>;
+  onLogout?: () => Promise<any>;
 };
 
 const AuthContext = createContext<AuthContextType>({});
 
 const useAuth = () => {
-    return useContext(AuthContext);
+  return useContext(AuthContext);
 };
 
 const AuthContextProvider = ({ children }: any) => {
-    const [authState, setAuthState] = useState<{
-        token: string | null;
-        isAuthenticated: boolean | null;
-    }>({
-        token: null,
-        isAuthenticated: null,
-    });
+  const [authState, setAuthState] = useState<{
+    token: string | null;
+    isAuthenticated: boolean | null;
+  }>({
+    token: null,
+    isAuthenticated: null,
+  });
 
-    const JWT_KEY = "token";
+  const JWT_KEY = "token";
 
-    useEffect(() => {
-        async function getKey(key: string) {
-            let result = await SecureStore.getItemAsync(key);
+  useEffect(() => {
+    async function getKey(key: string) {
+      let result = await SecureStore.getItemAsync(key);
 
-            if (result) {
-                axios.defaults.headers.common["Authorization"] = `Bearer ${result}`;
-                setAuthState({
-                    token: result,
-                    isAuthenticated: true,
-                });
-
-                alert("🔐 Here's your value 🔐 \n" + result);
-            }
-        }
-        getKey(JWT_KEY);
-    }, []);
-
-    const register = async (
-        email: string,
-        password: string,
-        username: string
-    ) => {
-        try {
-            const response = await axios.post(
-                "http://192.168.40.242:5173/api/v1/user/register",
-                {
-                    email,
-                    password,
-                    username,
-                },
-                {
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                }
-            );
-
-            console.log(response.data.body);
-            axios.defaults.headers.common[
-                "Authorization"
-            ] = `Bearer ${response.data.body.accessToken}`;
-
-            const setToken = await SecureStore.setItemAsync(
-                JWT_KEY,
-                response.data.body.accessToken
-            );
-            router.replace("/(app)/(tabs)");
-
-            console.log("SecureStore->:", setToken);
-        } catch (error) {
-            // return { error: true, message: (error as any).response.data.message };
-            console.error(error);
-        }
-    };
-
-    const login = async (email: string, password: string) => {
-        try {
-            const response = await axios.post(
-                "http://192.168.40.242:5173/api/v1/user/login",
-                { email, password }
-            );
-            console.info("response:", response.data.body);
-            setAuthState({
-                isAuthenticated: true,
-                token: response.data.body.accessToken,
-            });
-
-            axios.defaults.headers.common[
-                "Authorization"
-            ] = `Bearer ${response.data.body.accessToken}`;
-
-            await SecureStore.setItemAsync(JWT_KEY, response.data.body.accessToken);
-
-            router.replace("/(app)/(tabs)");
-
-            return response.data.body;;
-        } catch (error) {
-            console.error(error);
-            return { error: true, message: (error as any).response.data.message };
-        }
-    };
-
-    const logout = async () => {
-        await SecureStore.deleteItemAsync(JWT_KEY);
-        axios.defaults.headers.common["Authorization"] = "";
+      if (result) {
+        axios.defaults.headers.common["Authorization"] = `Bearer ${result}`;
         setAuthState({
-            isAuthenticated: null,
-            token: null,
+          token: result,
+          isAuthenticated: true,
         });
-    };
 
-    const value = {
-        onRegister: register,
-        onLogin: login,
-        onLogout: logout,
-        authState,
-    };
+        alert("🔐 Here's your value 🔐 \n" + result);
+      }
+    }
+    getKey(JWT_KEY);
+  }, []);
 
-    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+  const register = async (
+    email: string,
+    password: string,
+    username: string
+  ) => {
+    try {
+      const response = await axios.post(
+        "http://192.168.40.242:5173/api/v1/user/register",
+        {
+          email,
+          password,
+          username,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      console.log(response.data.body);
+      axios.defaults.headers.common[
+        "Authorization"
+      ] = `Bearer ${response.data.body.accessToken}`;
+
+      const setToken = await SecureStore.setItemAsync(
+        JWT_KEY,
+        response.data.body.accessToken
+      );
+      router.replace("/(app)/(tabs)");
+
+      console.log("SecureStore->:", setToken);
+    } catch (error) {
+      // return { error: true, message: (error as any).response.data.message };
+      console.error(error);
+    }
+  };
+
+  const login = async (email: string, password: string) => {
+    try {
+      const response = await axios.post(
+        "http://192.168.40.242:5173/api/v1/user/login",
+        { email, password }
+      );
+      console.info("response:", response.data.body);
+      setAuthState({
+        isAuthenticated: true,
+        token: response.data.body.accessToken,
+      });
+
+      axios.defaults.headers.common[
+        "Authorization"
+      ] = `Bearer ${response.data.body.accessToken}`;
+
+      await SecureStore.setItemAsync(JWT_KEY, response.data.body.accessToken);
+
+      router.replace("/(app)/(tabs)");
+
+      return response.data.body;
+    } catch (error) {
+      console.error(error);
+      return { error: true, message: (error as any).response.data.message };
+    }
+  };
+
+  const logout = async () => {
+    await SecureStore.deleteItemAsync(JWT_KEY);
+    axios.defaults.headers.common["Authorization"] = "";
+    setAuthState({
+      isAuthenticated: null,
+      token: null,
+    });
+  };
+
+  const value = {
+    onRegister: register,
+    onLogin: login,
+    onLogout: logout,
+    authState,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
 
 export { AuthContext, AuthContextProvider, useAuth };
