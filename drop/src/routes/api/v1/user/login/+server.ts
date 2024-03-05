74d187e55be73007e8df3334f93377e028e5ab6e
Bobie Fordjour McCamble Kofi <fordjourbobiem@gmail.com>
2024-03-05 00:59:20 +0000

    Refactor user login endpoint

--- drop/src/routes/api/v1/user/login/+server.ts
+++ drop/src/routes/api/v1/user/login/+server.ts
@@ -4,55 +4,54 @@ import { generateAccessToken } from '$lib/helpers/tokens.helper';
 import type { RequestHandler } from '@sveltejs/kit';
 
 export const POST: RequestHandler = async ({ request }) => {
-	const body = await request.json();
+    const body = await request.json();
 
-	const user = await prisma.user.findUnique({
-		where: {
-			email: body.email
-		},
-		include: {
-			userRefreshToken: true
-		}
-	});
+    const user = await prisma.user.findUnique({
+        where: {
+            email: body.email
+        },
+        include: {
+            userRefreshToken: true
+        }
+    });
 
-	if (!user) {
-		return new Response(JSON.stringify({ message: 'User does not exist' }), {
-			status: 400,
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			statusText: 'Bad Request'
-		});
-	}
+    if (!user) {
+        return new Response(JSON.stringify({ message: 'User does not exist' }), {
+            status: 400,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            statusText: 'Bad Request'
+        });
+    }
+    const verifyPassword = await comparePassword(body.password, user.password);
+    if (!verifyPassword) {
+        return new Response(JSON.stringify({ message: 'Invalid Password or User' }), {
+            status: 400,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            statusText: 'Bad Request'
+        });
+    }
 
-	const verifyPassword = await comparePassword(body.password, user.password);
-	if (!verifyPassword) {
-		return new Response(JSON.stringify({ message: 'Invalid Password or User' }), {
-			status: 400,
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			statusText: 'Bad Request'
-		});
-	}
+    const accessToken = await generateAccessToken({
+        email: user.email,
+        username: user.username,
+        Id: user.id
+    });
 
-	const accessToken = await generateAccessToken({
-		email: user.email,
-		username: user.username,
-		Id: user.id
-	});
-
-	return new Response(
-		JSON.stringify({
-			message: 'Successful',
-			body: { ...user, accessToken }
-		}),
-		{
-			status: 200,
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			statusText: 'OK'
-		}
-	);
+    return new Response(
+        JSON.stringify({
+            message: 'Successful',
+            body: { ...user, accessToken }
+        }),
+        {
+            status: 200,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            statusText: 'OK'
+        }
+    );
 };
