8b49f0fdfbd81d9865c7202f4a6cb3731adda552
Bobie Fordjour McCamble Kofi <fordjourbobiem@gmail.com>
2024-03-05 01:45:36 +0000

    Fix import statement in _layout.tsx

--- drop_app/app/(app)/_layout.tsx
+++ drop_app/app/(app)/_layout.tsx
@@ -1,10 +1,9 @@
 import { useFonts } from "expo-font";
 import { SplashScreen, Stack } from "expo-router";
 import { useEffect } from "react";
-
 export {
   // Catch any errors thrown by the Layout component.
-  ErrorBoundary
+  ErrorBoundary,
 } from "expo-router";
 
 export const unstable_settings = {
