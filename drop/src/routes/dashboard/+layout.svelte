ebaf54f8599ac38e321f68584b93db5d18d9dae8
Bobie Fordjour McCamble Kofi <fordjourbobiem@gmail.com>
2024-03-22 19:21:55 +0000

    Resizable dashboard
    dashboard can be resized to suit any style for optimal screen view

--- drop/src/routes/dashboard/+layout.svelte
+++ drop/src/routes/dashboard/+layout.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
+	import { cn } from '$lib/utils';
 	import * as Resizable from '@/components/resizable/index.js';
 
+	export let defaultLayout = [265, 835];
 	export let defaultCollasped = false;
 
 	let isCollapsed = defaultCollasped;
+
+	function onLayoutChange(sizes: number[]) {
+		document.cookie = `PaneForge:Layout=${JSON.stringify(sizes)}`;
+	}
 
 	function onCollapse() {
 		isCollapsed = true;
@@ -16,14 +22,31 @@
 	}
 </script>
 
-<Resizable.PaneGroup direction="horizontal" class="min-h-screen min-w-full rounded-lg border">
-	<Resizable.Pane defaultSize={15}>
-		<div>navigation bar</div>
+<Resizable.PaneGroup
+	direction="horizontal"
+	{onLayoutChange}
+	class="min-h-screen min-w-full rounded-lg border"
+>
+	<Resizable.Pane
+		defaultSize={defaultLayout[0]}
+		{onCollapse}
+		{onExpand}
+		minSize={15}
+		maxSize={20}
+	>
+		<div
+			class={cn(
+				'flex h-[52px] items-center justify-center',
+				isCollapsed ? 'h-[52px]' : 'px-2'
+			)}
+		>
+			header
+		</div>
 	</Resizable.Pane>
 
 	<Resizable.Handle withHandle />
 
-	<Resizable.Pane defaultSize={85}>
+	<Resizable.Pane defaultSize={defaultLayout[1]}>
 		<slot />
 	</Resizable.Pane>
 </Resizable.PaneGroup>
