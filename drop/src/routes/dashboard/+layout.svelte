6e3160be66c1c37b2bf0d719d30b9e9a6b3787dd
Bobie Fordjour McCamble Kofi <fordjourbobiem@gmail.com>
2024-03-24 01:10:22 +0000

    dashboard component
    addition of a resizable dashboard sidebar
    tooltip for hovering on the icons to show the infomation

--- drop/src/routes/dashboard/+layout.svelte
+++ drop/src/routes/dashboard/+layout.svelte
@@ -1,52 +1,8 @@
 <script lang="ts">
-	import { cn } from '$lib/utils';
-	import * as Resizable from '@/components/resizable/index.js';
+	import type { LayoutData } from './$types';
+	import DaBoard from './(component)/daboard.svelte';
 
-	export let defaultLayout = [265, 835];
-	export let defaultCollasped = false;
-
-	let isCollapsed = defaultCollasped;
-
-	function onLayoutChange(sizes: number[]) {
-		document.cookie = `PaneForge:Layout=${JSON.stringify(sizes)}`;
-	}
-
-	function onCollapse() {
-		isCollapsed = true;
-		document.cookie = `PaneForge:Layout=${true}`;
-	}
-
-	function onExpand() {
-		isCollapsed = false;
-		document.cookie = `PaneForge:Layout=${false}`;
-	}
+	export let data: LayoutData;
 </script>
 
-<Resizable.PaneGroup
-	direction="horizontal"
-	{onLayoutChange}
-	class="min-h-screen min-w-full rounded-lg border"
->
-	<Resizable.Pane
-		defaultSize={defaultLayout[0]}
-		{onCollapse}
-		{onExpand}
-		minSize={15}
-		maxSize={20}
-	>
-		<div
-			class={cn(
-				'flex h-[52px] items-center justify-center',
-				isCollapsed ? 'h-[52px]' : 'px-2'
-			)}
-		>
-			header
-		</div>
-	</Resizable.Pane>
-
-	<Resizable.Handle withHandle />
-
-	<Resizable.Pane defaultSize={defaultLayout[1]}>
-		<slot />
-	</Resizable.Pane>
-</Resizable.PaneGroup>
+<DaBoard defaultCollapsed={data.collapsed} defaultLayout={data.layout} navCollapsedSize={4} />
---
+++ drop/src/routes/dashboard/config.ts
@@ -0,0 +1,40 @@
+import type { Icon } from "lucide-svelte";
+import { type ComponentType } from "svelte";
+import * as Icons from "./icons.js";
+
+export type Route = {
+    name: string;
+    url: string;
+    icon: ComponentType<Icon>;
+    label: string;
+    variant: "default" | "ghost";
+}
+
+export const menuRoutes: Route[] = [
+    {
+        name: "My Dashboard",
+        url: "/dash",
+        label: "128",
+        icon: Icons.Archive,
+        variant: "default",
+    },
+    {
+        name: "Drafts",
+        url: "#",
+        label: "9",
+        icon: Icons.CircleAlert,
+        variant: "ghost",
+    }, {
+        name: "Hello",
+        url: "/dash/hello",
+        icon: Icons.Ampersand,
+        variant: "ghost",
+        label: "New"
+    }, {
+        name: "Hello2",
+        url: "/dash/hello2",
+        icon: Icons.Airplay,
+        variant: "ghost",
+        label: "New"
+    }
+]
