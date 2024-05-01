d2037ec26f77ecd85eca07ebc258ef7d46eca0e3
Bobie Fordjour McCamble Kofi <fordjourbobiem@gmail.com>
2024-05-01 16:04:01 +0000

    sign in and out test

--- v2/drop/app/(app)/(root)/two.tsx
+++ v2/drop/app/(app)/(root)/two.tsx
@@ -1,10 +1,37 @@
+import { SignedIn, SignedOut } from "@clerk/clerk-expo";
+import { Link, Stack } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
+// import { styles } from "../../components/Styles";
 
-export default function two() {
+export default function Page() {
 	return (
 		<View>
-			<Text>two</Text>
+			<Stack.Screen
+				options={{
+					title: "Home",
+				}}
+			/>
+			<Text>Welcome!</Text>
+			<SignedIn>
+				<Link href={"/profile"} asChild>
+					<TouchableOpacity>
+						<Text>Go to Profile</Text>
+					</TouchableOpacity>
+				</Link>
+			</SignedIn>
+			<SignedOut>
+				<Link href={"/login"}>
+					<TouchableOpacity>
+						<Text>Sign In</Text>
+					</TouchableOpacity>
+				</Link>
+				<Link href={"/register"} asChild>
+					<TouchableOpacity>
+						<Text>Sign Up</Text>
+					</TouchableOpacity>
+				</Link>
+			</SignedOut>
 		</View>
 	);
 }
