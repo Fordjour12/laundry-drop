01f2f66da027d2dea8e9734ee5a1579ad04f19f4
Bobie Fordjour McCamble Kofi <fordjourbobiem@gmail.com>
2024-05-12 15:11:49 +0000

    Generation a hash
    a Hash for authentication

--- v2/drop-spf/internal/helper/types.go
+++ v2/drop-spf/internal/helper/types.go
@@ -1,6 +1,10 @@
 package helper
 
-import "time"
+import (
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+)
 
 type UserAccount struct {
 	Id         int       `json:"id"`
@@ -9,6 +13,12 @@ type UserAccount struct {
 	Password   string    `json:"password"`
 	Created_at time.Time `json:"created_at"`
 	Updated_at time.Time `json:"updated_at"`
+}
+
+type UserAccountReq struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
 
 type LaundryCompany struct {
@@ -29,4 +39,17 @@ type Employee struct {
 	Role       string    `json:"role"`
 	Created_at time.Time `json:"created_at"`
 	Updated_at time.Time `json:"updated_at"`
+}
+
+func NewUserAccountRequest(username, email, password string) (*UserAccount, error) {
+	passHash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+
+	return &UserAccount{
+		Username: username,
+		Email:    email,
+		Password: string(passHash),
+	}, nil
 }
