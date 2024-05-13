5b0b58124f4705a386d854949fdb54cfda70ecc2
Bobie Fordjour McCamble Kofi <fordjourbobiem@gmail.com>
2024-05-13 14:41:15 +0000

    welcome screen

--- v2/drop/app/(modal)/index.tsx
+++ v2/drop/app/(modal)/index.tsx
@@ -1,54 +1,102 @@
-import * as Location from "expo-location";
-import React, { useEffect, useState } from "react";
-import { Dimensions, StyleSheet } from "react-native";
-import MapView from "react-native-maps";
+import { Button, ButtonOutline } from "@/components/ui/Button";
+import {
+	BottomSheetModal,
+	BottomSheetModalProvider,
+	BottomSheetView,
+} from "@gorhom/bottom-sheet";
+import { router } from "expo-router";
+import { useCallback, useMemo, useRef } from "react";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
-	const [location, setLocation] = useState({});
-  const [errorMsg, setErrorMsg] = useState<string>("");
+	const backgroundImage = require("../../assets/images/laundry.jpg");
+	// ref
+	const bottomSheetModalRef = useRef<BottomSheetModal>(null);
 
-  useEffect(() => {
-    (async () => {
-      
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-        return;
-      }
+	// variables
+	const snapPoints = useMemo(() => ["25%", "50%"], []);
 
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-    })();
-  }, []);
-
-  let text = 'Waiting..';
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
-  }
+	// callbacks
+	const handlePresentModalPress = useCallback(() => {
+		bottomSheetModalRef.current?.present();
+	}, []);
+	const handleSheetChanges = useCallback((index: number) => {
+		console.log("handleSheetChanges", index);
+	}, []);
 
 	return (
-			 <MapView
-				style={{
-					width: Dimensions.get('screen').width,
-					height: Dimensions.get('screen').height
-				}} /> 
-				
+		<BottomSheetModalProvider>
+			<ImageBackground source={backgroundImage} style={styles.container}>
+				<View style={{ flex: 1 }} />
+				<View>
+					<Button
+						title="Continue"
+						onPress={() => router.push("/(modal)/register")}
+					/>
+					<View style={{ marginVertical: 8 }} />
+					<View>
+						<ButtonOutline
+							title="Login to your account"
+							onPress={handlePresentModalPress}
+						/>
+
+						<BottomSheetModal
+							animateOnMount={true}
+							ref={bottomSheetModalRef}
+							index={1}
+							snapPoints={snapPoints}
+							onChange={handleSheetChanges}
+						>
+							<BottomSheetView style={styles.contentContainer}>
+								<Text>Awesome 🎉</Text>
+							</BottomSheetView>
+						</BottomSheetModal>
+					</View>
+				</View>
+			</ImageBackground>
+		</BottomSheetModalProvider>
 	);
+
+	// return (
+	// 	<ImageBackground source={backgroundImage} style={styles.container}>
+	// 		<View style={{ flex: 1 }} />
+
+	// 		<View style={{ flex: 0.3 }}>
+	// 			{/* <Text>Welcome to Drop!</Text> */}
+
+	// 			<View style={{ marginHorizontal: 16 }}>
+	// 				<Button
+	// 					title="Continue"
+	// 					onPress={() => router.push("/(modal)/register")}
+	// 				/>
+	// 				<View style={{ marginVertical: 8 }} />
+	// 				<ButtonOutline
+	// 					title="Login to your account"
+	// 					onPress={() => {
+	// 						router.push("/(modal)/sign-in");
+	// 					}}
+	// 				/>
+	// 			</View>
+	// 		</View>
+	// 	</ImageBackground>
+	// );
 }
 
-
+// const styles = StyleSheet.create({
+// 	container: {
+// 		flex: 1,
+// 	},
+// });
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
+		padding: 24,
 		justifyContent: "center",
-		alignItems: "center",
-
+		backgroundColor: "grey",
 	},
-	map: {
-		...StyleSheet.absoluteFillObject,
+	contentContainer: {
+		flex: 1,
+		alignItems: "center",
 	},
 });
-
