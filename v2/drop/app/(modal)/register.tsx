48c9d91417d5dc6a98f97d92508d5c8b88106ce0
Bobie Fordjour McCamble Kofi <fordjourbobiem@gmail.com>
2024-05-13 15:51:42 +0000

    Register screen

--- v2/drop/app/(modal)/register.tsx
+++ v2/drop/app/(modal)/register.tsx
@@ -1,8 +1,16 @@
-import { Button } from "@/components/ui/Button";
+import { Button, ButtonWithIcon } from "@/components/ui/Button";
 import TextInputWithLabel from "@/components/ui/TextInput";
+import Separator from "@/components/ui/separator";
 import { AppColor } from "@/constants/Colors";
+import axios from "axios";
 import React from "react";
 import { ImageBackground, StyleSheet, Text, View } from "react-native";
+
+type RegisterProps = {
+	username: string;
+	email: string;
+	password: string;
+};
 
 export default function Register() {
 	const backgroundImage = require("../../assets/images/laundry.jpg");
@@ -11,9 +19,34 @@ export default function Register() {
 	const [email, setEmail] = React.useState("");
 	const [password, setPassword] = React.useState("");
 
+	const API_URI = process.env.EXPO_PUBLIC_API_URL as string;
+
 	const handleInputChange =
-		(setter: React.Dispatch<React.SetStateAction<string>>) => (text: string) =>
-			setter(text);
+		(setter: React.Dispatch<React.SetStateAction<string>>) =>
+			(text: string) => {
+				setter(text);
+				// console.log(text);
+			};
+
+	const registerUserAccount = async ({
+		username,
+		email,
+		password,
+	}: RegisterProps) => {
+		await axios.post(
+			`${API_URI}create-account`,
+			{
+				username: username,
+				email: email,
+				password: password,
+			},
+			{
+				headers: {
+					"Content-Type": "application/json",
+				},
+			},
+		);
+	};
 
 	return (
 		<ImageBackground
@@ -44,8 +77,18 @@ export default function Register() {
 						onChangeText={handleInputChange(setPassword)}
 						value={password}
 					/>
+					<Button
+						title="Sign Up"
+						onPress={() => registerUserAccount({ username, email, password })}
+					/>
+					<View style={styles.separatorView}>
+						<Separator height={2} />
+						<Text style={styles.text}>or</Text>
+						<Separator height={2} />
+					</View>
+
 					<View style={{ marginTop: 20, marginBottom: 10 }}>
-						<Button title="Sign Up" />
+						<ButtonWithIcon title="Continue with Google" iconName="google" />
 					</View>
 				</View>
 			</View>
@@ -63,8 +106,8 @@ const styles = StyleSheet.create({
 	blurBackdrop: {
 		// ...StyleSheet.absoluteFillObject,
 		backgroundColor: "hsla(173, 32%, 32%,0.8)",
-		borderTopEndRadius: 20,
-		borderTopStartRadius: 20,
+		borderTopEndRadius: 25,
+		borderTopStartRadius: 25,
 		flex: 1,
 		padding: 14,
 	},
@@ -78,23 +121,12 @@ const styles = StyleSheet.create({
 		fontWeight: "600",
 		color: AppColor[300],
 	},
+	separatorView: {
+		flexDirection: "row",
+		gap: 10,
+		alignItems: "center",
+		marginVertical: 20,
+	},
+
 });
 
-// const styles = StyleSheet.create({
-// 	container: {
-// 		flex: 1,
-// 		paddingHorizontal: 16,
-// 		paddingTop: 30,
-
-// 	},
-// 	separatorView: {
-// 		flexDirection: "row",
-// 		gap: 10,
-// 		alignItems: "center",
-// 		marginVertical: 30,
-// 	},
-// 	text: {
-// 		fontWeight: "600",
-// 		color: AppColor[300],
-// 	},
-// });
