a570fab5448255c5fe58240495faf0ddf441c703
Bobie Fordjour McCamble Kofi <fordjourbobiem@gmail.com>
2024-05-13 15:51:52 +0000

    Sign in

--- v2/drop/app/(modal)/sign-in.tsx
+++ v2/drop/app/(modal)/sign-in.tsx
@@ -1,11 +1,11 @@
 import { ThemedText } from "@/components/ThemedText";
-import React from "react";
 import { View } from "react-native";
 
 export default function SignIn() {
 	return (
 		<View>
-			<ThemedText>sign-in</ThemedText>
+			<ThemedText>Signed In </ThemedText>
 		</View>
+
 	);
 }
