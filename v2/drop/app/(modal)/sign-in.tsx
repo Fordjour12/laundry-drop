062efe46328a181bf1ccf54f7ef7e1f5bcee08b7
Bobie Fordjour McCamble Kofi <fordjourbobiem@gmail.com>
2024-05-14 13:43:16 +0000

    sign in screen

--- v2/drop/app/(modal)/sign-in.tsx
+++ v2/drop/app/(modal)/sign-in.tsx
@@ -1,11 +1,136 @@
-import { ThemedText } from "@/components/ThemedText";
-import { View } from "react-native";
+import { Button, ButtonWithIcon } from "@/components/ui/Button";
+import TextInputWithLabel from "@/components/ui/TextInput";
+import Separator from "@/components/ui/separator";
+import Spinner from "@/components/ui/spinner";
+import { AppColor } from "@/constants/Colors";
+import axios from "axios";
+import React from "react";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
+
+type SignInProps = {
+	email: string;
+	password: string;
+};
 
 export default function SignIn() {
-	return (
-		<View>
-			<ThemedText>Signed In </ThemedText>
-		</View>
+	const backgroundImage = require("../../assets/images/laundry.jpg");
 
+	const [username, setUsername] = React.useState("");
+	const [email, setEmail] = React.useState("");
+	const [password, setPassword] = React.useState("");
+	const [isLoading, setIsLoading] = React.useState(false);
+
+	const API_URI = process.env.EXPO_PUBLIC_API_URL as string;
+
+	const handleInputChange =
+		(setter: React.Dispatch<React.SetStateAction<string>>) =>
+		(text: string) => {
+			setter(text);
+			// console.log(text);
+		};
+
+	const signInUserAccount = async ({ email, password }: SignInProps) => {
+		setIsLoading(true);
+		try {
+			const res = await axios.post(
+				`${API_URI}login-account`,
+				{
+					email: email,
+					password: password,
+				},
+				{
+					headers: {
+						"Content-Type": "application/json",
+					},
+				},
+			);
+			console.log(res.data);
+		} catch (error) {
+			console.error(error);
+		} finally {
+			setIsLoading(false);
+		}
+	};
+
+	return (
+		<ImageBackground
+			source={backgroundImage}
+			imageStyle={styles.image}
+			style={styles.container}
+		>
+			<View style={{ flex: 0.5 }} />
+			<View style={styles.blurBackdrop}>
+				<Text style={styles.heading}>Login your account</Text>
+				<View style={{ flex: 0.7 }}>
+					<TextInputWithLabel
+						label="Email"
+						style={styles.text}
+						onChangeText={handleInputChange(setEmail)}
+						value={email}
+					/>
+					<TextInputWithLabel
+						label="Password"
+						secureTextEntry
+						style={styles.text}
+						onChangeText={handleInputChange(setPassword)}
+						value={password}
+					/>
+
+					{isLoading ? (
+						<Spinner />
+					) : (
+						<Button
+							title="Sign Up"
+							onPress={() => signInUserAccount({ email, password })}
+						/>
+					)}
+
+					<View style={styles.separatorView}>
+						<Separator height={3} color={AppColor[100]} />
+						<Text style={styles.text}>or</Text>
+						<Separator height={3} color={AppColor[100]} />
+					</View>
+
+					<Spinner />
+
+					<View style={{ marginTop: 20, marginBottom: 10 }}>
+						<ButtonWithIcon title="Continue with Google" iconName="google" />
+					</View>
+				</View>
+			</View>
+		</ImageBackground>
 	);
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+	},
+	image: {
+		opacity: 1,
+	},
+	blurBackdrop: {
+		// ...StyleSheet.absoluteFillObject,
+		backgroundColor: "hsla(173, 32%, 32%,0.8)",
+		borderTopEndRadius: 25,
+		borderTopStartRadius: 25,
+		flex: 1,
+		padding: 14,
+	},
+	heading: {
+		fontSize: 30,
+		fontWeight: "900",
+		color: AppColor[300],
+		paddingBottom: 20,
+	},
+	text: {
+		fontWeight: "600",
+		color: AppColor[300],
+	},
+	separatorView: {
+		flexDirection: "row",
+		gap: 10,
+		alignItems: "center",
+		marginVertical: 20,
+	},
+});
