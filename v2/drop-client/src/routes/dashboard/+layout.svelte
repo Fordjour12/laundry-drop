3856184d81ce2bac69d36709d93319dcc63d87e7
Bobie Fordjour McCamble Kofi <fordjourbobiem@gmail.com>
2024-05-30 07:43:57 +0000

    session

--- v2/drop-client/src/routes/dashboard/+layout.svelte
+++ v2/drop-client/src/routes/dashboard/+layout.svelte
@@ -3,9 +3,11 @@
 	import { routesConfig } from './routesConfig';
 </script>
 
+
 <div class="flex min-h-screen w-full flex-col bg-app-mixed-100">
 	<aside class="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-app-mixed-300 sm:flex">
 		<NavBar />
 	</aside>
+
 	<slot />
 </div>
