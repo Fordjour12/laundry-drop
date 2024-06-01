a8ac1d671d48dfc905102b44fb7b35ffeb718b8c
Bobie Fordjour McCamble Kofi <fordjourbobiem@gmail.com>
2024-06-01 16:36:19 +0000

    tab pages

---
+++ v2/drop/app/(tabs)/laundry/[details].tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function LaundryDetails() {
+	return (
+		<View>
+			<Text>Laundry Details Page</Text>
+		</View>
+	);
+}
