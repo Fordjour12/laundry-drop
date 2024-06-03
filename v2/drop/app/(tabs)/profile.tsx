735ad7230a4584e257069f74c3d59624ad59f0be
Bobie Fordjour McCamble Kofi <fordjourbobiem@gmail.com>
2024-06-03 01:27:48 +0000

    chore: Profile page update

--- v2/drop/app/(tabs)/profile.tsx
+++ v2/drop/app/(tabs)/profile.tsx
@@ -1,175 +1,397 @@
-import { FontAwesome } from "@expo/vector-icons";
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
-	Button,
-	ScrollView,
 	StyleSheet,
-	Switch,
+	SafeAreaView,
+	View,
+	ScrollView,
 	Text,
 	TouchableOpacity,
-	View,
-} from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+	Switch,
+	Image,
+} from 'react-native';
+import FeatherIcon from '@expo/vector-icons/Feather';
 
-export default function Profile() {
-	const user = {
-		name: "John Doe",
-		email: "john.doe@example.com",
-		avatar: "https://randomuser.me/api/portraits/men/1.jpg",
-	};
-
-	const [isDarkMode, setIsDarkMode] = useState(false);
+export default function Example() {
+	const [form, setForm] = useState({
+		emailNotifications: true,
+		pushNotifications: false,
+	});
 
 	return (
-		<SafeAreaView>
-			{/* <Text>Profile</Text> */}
-			<ScrollView>
-				<View style={styles.container}>
-					{/* <Image source={{ uri: user.avatar }} style={styles.avatar} /> */}
-					<View style={styles.avatar} />
-					<Text style={styles.name}>{user.name}</Text>
-					<Text style={styles.email}>{user.email}</Text>
-					<View style={styles.icons}>
-						<FontAwesome
-							name="facebook-square"
-							size={30}
-							color="#3b5998"
-							style={styles.icon}
-						/>
-						<FontAwesome
-							name="twitter"
-							size={30}
-							color="#00acee"
-							style={styles.icon}
-						/>
-						<FontAwesome
-							name="instagram"
-							size={30}
-							color="#C13584"
-							style={styles.icon}
-						/>
+		<SafeAreaView style={{ flex: 1, backgroundColor: '#f8f8f8' }}>
+			<View style={styles.container}>
+				<View style={styles.header}>
+					<View style={styles.headerAction}>
+						<TouchableOpacity
+							onPress={() => {
+								// handle onPress
+							}}>
+							<FeatherIcon
+								color="#000"
+								name="arrow-left"
+								size={24} />
+						</TouchableOpacity>
 					</View>
-					<View style={styles.buttonContainer}>
-						<Button title="Edit Profile" onPress={() => {}} />
+
+					<Text style={styles.headerTitle}>Settings</Text>
+
+					<View style={[styles.headerAction, { alignItems: 'flex-end' }]}>
+						<TouchableOpacity
+							onPress={() => {
+								// handle onPress
+							}}>
+							<FeatherIcon
+								color="#000"
+								name="more-vertical"
+								size={24} />
+						</TouchableOpacity>
 					</View>
 				</View>
 
-				<View style={[styles.container_as]}>
-					<View style={[styles.section]}>
-						<Text style={[styles.sectionTitle]}>Appearance</Text>
-						<View style={styles.item}>
-							<Text style={[styles.itemText]}>Dark Mode</Text>
-							<Switch value={isDarkMode} />
+				<ScrollView contentContainerStyle={styles.content}>
+					<View style={[styles.section, { paddingTop: 4 }]}>
+						<Text style={styles.sectionTitle}>Account</Text>
+
+						<View style={styles.sectionBody}>
+							<TouchableOpacity
+								onPress={() => {
+									// handle onPress
+								}}
+								style={styles.profile}>
+								<Image
+									alt=""
+									source={{
+										uri: 'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2.5&w=256&h=256&q=80',
+									}}
+									style={styles.profileAvatar} />
+
+								<View style={styles.profileBody}>
+									<Text style={styles.profileName}>John Doe</Text>
+
+									<Text style={styles.profileHandle}>
+										john.doe@mail.com
+									</Text>
+								</View>
+
+								<FeatherIcon
+									color="#bcbcbc"
+									name="chevron-right"
+									size={22} />
+							</TouchableOpacity>
 						</View>
 					</View>
 
-					<View style={[styles.section, isDarkMode && styles.darkSection]}>
-						<Text style={[styles.sectionTitle]}>Notifications</Text>
-						<TouchableOpacity style={styles.item}>
-							<Text style={[styles.itemText]}>Notification Settings</Text>
-						</TouchableOpacity>
+					<View style={styles.section}>
+						<Text style={styles.sectionTitle}>Preferences</Text>
+
+						<View style={styles.sectionBody}>
+							<View style={[styles.rowWrapper, styles.rowFirst]}>
+								<TouchableOpacity
+									onPress={() => {
+										// handle onPress
+									}}
+									style={styles.row}>
+									<Text style={styles.rowLabel}>Language</Text>
+
+									<View style={styles.rowSpacer} />
+
+									<Text style={styles.rowValue}>English</Text>
+
+									<FeatherIcon
+										color="#bcbcbc"
+										name="chevron-right"
+										size={19} />
+								</TouchableOpacity>
+							</View>
+
+							<View style={styles.rowWrapper}>
+								<TouchableOpacity
+									onPress={() => {
+										// handle onPress
+									}}
+									style={styles.row}>
+									<Text style={styles.rowLabel}>Location</Text>
+
+									<View style={styles.rowSpacer} />
+
+									<Text style={styles.rowValue}>Los Angeles, CA</Text>
+
+									<FeatherIcon
+										color="#bcbcbc"
+										name="chevron-right"
+										size={19} />
+								</TouchableOpacity>
+							</View>
+
+							<View style={styles.rowWrapper}>
+								<View style={styles.row}>
+									<Text style={styles.rowLabel}>Email Notifications</Text>
+
+									<View style={styles.rowSpacer} />
+
+									<Switch
+										onValueChange={emailNotifications =>
+											setForm({ ...form, emailNotifications })
+										}
+										style={{ transform: [{ scaleX: 0.95 }, { scaleY: 0.95 }] }}
+										value={form.emailNotifications} />
+								</View>
+							</View>
+
+							<View style={[styles.rowWrapper, styles.rowLast]}>
+								<View style={styles.row}>
+									<Text style={styles.rowLabel}>Push Notifications</Text>
+
+									<View style={styles.rowSpacer} />
+
+									<Switch
+										onValueChange={pushNotifications =>
+											setForm({ ...form, pushNotifications })
+										}
+										style={{ transform: [{ scaleX: 0.95 }, { scaleY: 0.95 }] }}
+										value={form.pushNotifications} />
+								</View>
+							</View>
+						</View>
 					</View>
 
-					<View style={[styles.section]}>
-						<Text style={[styles.sectionTitle]}>Security</Text>
-						<TouchableOpacity style={styles.item}>
-							<Text style={[styles.itemText]}>Change Password</Text>
-						</TouchableOpacity>
+					<View style={styles.section}>
+						<Text style={styles.sectionTitle}>Resources</Text>
+
+						<View style={styles.sectionBody}>
+							<View style={[styles.rowWrapper, styles.rowFirst]}>
+								<TouchableOpacity
+									onPress={() => {
+										// handle onPress
+									}}
+									style={styles.row}>
+									<Text style={styles.rowLabel}>Contact Us</Text>
+
+									<View style={styles.rowSpacer} />
+
+									<FeatherIcon
+										color="#bcbcbc"
+										name="chevron-right"
+										size={19} />
+								</TouchableOpacity>
+							</View>
+
+							<View style={styles.rowWrapper}>
+								<TouchableOpacity
+									onPress={() => {
+										// handle onPress
+									}}
+									style={styles.row}>
+									<Text style={styles.rowLabel}>Report Bug</Text>
+
+									<View style={styles.rowSpacer} />
+
+									<FeatherIcon
+										color="#bcbcbc"
+										name="chevron-right"
+										size={19} />
+								</TouchableOpacity>
+							</View>
+
+							<View style={styles.rowWrapper}>
+								<TouchableOpacity
+									onPress={() => {
+										// handle onPress
+									}}
+									style={styles.row}>
+									<Text style={styles.rowLabel}>Rate in App Store</Text>
+
+									<View style={styles.rowSpacer} />
+
+									<FeatherIcon
+										color="#bcbcbc"
+										name="chevron-right"
+										size={19} />
+								</TouchableOpacity>
+							</View>
+
+							<View style={[styles.rowWrapper, styles.rowLast]}>
+								<TouchableOpacity
+									onPress={() => {
+										// handle onPress
+									}}
+									style={styles.row}>
+									<Text style={styles.rowLabel}>Terms and Privacy</Text>
+
+									<View style={styles.rowSpacer} />
+
+									<FeatherIcon
+										color="#bcbcbc"
+										name="chevron-right"
+										size={19} />
+								</TouchableOpacity>
+							</View>
+						</View>
 					</View>
 
-					<View style={[styles.section]}>
-						<Text style={[styles.sectionTitle]}>Help</Text>
-						<TouchableOpacity style={styles.item}>
-							<Text style={[styles.itemText]}>Help Center</Text>
-						</TouchableOpacity>
+					<View style={styles.section}>
+						<View style={styles.sectionBody}>
+							<View
+								style={[
+									styles.rowWrapper,
+									styles.rowFirst,
+									styles.rowLast,
+									{ alignItems: 'center' },
+								]}>
+								<TouchableOpacity
+									onPress={() => {
+										// handle onPress
+									}}
+									style={styles.row}>
+									<Text style={[styles.rowLabel, styles.rowLabelLogout]}>
+										Log Out
+									</Text>
+								</TouchableOpacity>
+							</View>
+						</View>
 					</View>
 
-					<View style={[styles.section]}>
-						<Text style={[styles.sectionTitle]}>About</Text>
-						<TouchableOpacity style={styles.item}>
-							<Text style={[styles.itemText]}>About Us</Text>
-						</TouchableOpacity>
-					</View>
-				</View>
-			</ScrollView>
+					<Text style={styles.contentFooter}>App Version 2.24 #50491</Text>
+				</ScrollView>
+			</View>
 		</SafeAreaView>
 	);
 }
 
 const styles = StyleSheet.create({
 	container: {
-		alignItems: "center",
-		justifyContent: "center",
-		backgroundColor: "#f5f5f5",
+		padding: 0,
+		flexGrow: 1,
+		flexShrink: 1,
+		flexBasis: 0,
 	},
-	avatar: {
-		width: 100,
-		height: 100,
-		borderRadius: 50,
-		marginBottom: 20,
-		backgroundColor: "gray",
+	/** Header */
+	header: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+		width: '100%',
+		paddingHorizontal: 16,
 	},
-	name: {
-		fontSize: 24,
-		fontWeight: "bold",
-		marginBottom: 10,
+	headerAction: {
+		width: 40,
+		height: 40,
+		alignItems: 'flex-start',
+		justifyContent: 'center',
 	},
-	email: {
-		fontSize: 16,
-		color: "gray",
-		marginBottom: 20,
+	headerTitle: {
+		fontSize: 19,
+		fontWeight: '600',
+		color: '#000',
 	},
-	icons: {
-		flexDirection: "row",
-		marginBottom: 20,
+	/** Content */
+	content: {
+		paddingHorizontal: 16,
 	},
-	icon: {
-		marginHorizontal: 10,
+	contentFooter: {
+		marginTop: 24,
+		fontSize: 13,
+		fontWeight: '500',
+		textAlign: 'center',
+		color: '#a69f9f',
 	},
-	buttonContainer: {
-		marginTop: 20,
-		width: "80%",
-	},
-	// testing
-	container_as: {
-		// flex: 1,
-		padding: 20,
-		backgroundColor: "#f5f5f5",
-	},
-	darkContainer: {
-		backgroundColor: "#333",
-	},
+	/** Section */
 	section: {
-		marginBottom: 30,
-	},
-	darkSection: {
-		backgroundColor: "#444",
+		paddingVertical: 12,
 	},
 	sectionTitle: {
-		fontSize: 20,
-		fontWeight: "bold",
-		marginBottom: 10,
-		color: "#000",
+		margin: 8,
+		marginLeft: 12,
+		fontSize: 13,
+		letterSpacing: 0.33,
+		fontWeight: '500',
+		color: '#a69f9f',
+		textTransform: 'uppercase',
 	},
-	darkSectionTitle: {
-		color: "#fff",
+	sectionBody: {
+		borderRadius: 12,
+		shadowColor: '#000',
+		shadowOffset: {
+			width: 0,
+			height: 1,
+		},
+		shadowOpacity: 0.2,
+		shadowRadius: 1.41,
+		elevation: 2,
 	},
-	item: {
-		flexDirection: "row",
-		justifyContent: "space-between",
-		alignItems: "center",
-		paddingVertical: 15,
-		borderBottomWidth: 1,
-		borderBottomColor: "#ccc",
+	/** Profile */
+	profile: {
+		padding: 12,
+		backgroundColor: '#fff',
+		borderRadius: 12,
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'flex-start',
 	},
-	darkItem: {
-		borderBottomColor: "#555",
+	profileAvatar: {
+		width: 60,
+		height: 60,
+		borderRadius: 9999,
+		marginRight: 12,
 	},
-	itemText: {
+	profileBody: {
+		marginRight: 'auto',
+	},
+	profileName: {
+		fontSize: 18,
+		fontWeight: '600',
+		color: '#292929',
+	},
+	profileHandle: {
+		marginTop: 2,
 		fontSize: 16,
-		color: "#000",
+		fontWeight: '400',
+		color: '#858585',
 	},
-	darkItemText: {
-		color: "#fff",
+	/** Row */
+	row: {
+		height: 44,
+		width: '100%',
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'flex-start',
+		paddingRight: 12,
+	},
+	rowWrapper: {
+		paddingLeft: 16,
+		backgroundColor: '#fff',
+		borderTopWidth: 1,
+		borderColor: '#f0f0f0',
+	},
+	rowFirst: {
+		borderTopLeftRadius: 12,
+		borderTopRightRadius: 12,
+	},
+	rowLabel: {
+		fontSize: 16,
+		letterSpacing: 0.24,
+		color: '#000',
+	},
+	rowSpacer: {
+		flexGrow: 1,
+		flexShrink: 1,
+		flexBasis: 0,
+	},
+	rowValue: {
+		fontSize: 16,
+		fontWeight: '500',
+		color: '#ababab',
+		marginRight: 4,
+	},
+	rowLast: {
+		borderBottomLeftRadius: 12,
+		borderBottomRightRadius: 12,
+	},
+	rowLabelLogout: {
+		width: '100%',
+		textAlign: 'center',
+		fontWeight: '600',
+		color: '#dc2626',
 	},
 });
+
