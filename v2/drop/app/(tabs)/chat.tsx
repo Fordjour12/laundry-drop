9ca616af4cee49ef3e9fa0769ace45df79f7cfdb
Bobie Fordjour McCamble Kofi <fordjourbobiem@gmail.com>
2024-06-04 01:23:28 +0000

    chore: Updating chat

--- v2/drop/app/(tabs)/chat.tsx
+++ v2/drop/app/(tabs)/chat.tsx
@@ -1,10 +1,32 @@
+import { Link } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
+import { Pressable, StyleSheet, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Chat() {
 	return (
-		<View>
+		<SafeAreaView>
 			<Text>chat</Text>
-		</View>
+
+			<Link href={"/category/washing"} push asChild>
+				<Pressable style={styles.button}>
+					<Text style={styles.buttonText}>Go to Washing</Text>
+				</Pressable>
+			</Link>
+		</SafeAreaView>
 	);
 }
+
+const styles = StyleSheet.create({
+	button: {
+		backgroundColor: "#007BFF",
+		padding: 10,
+		borderRadius: 5,
+		alignItems: "center",
+		marginHorizontal: 20,
+	},
+	buttonText: {
+		color: "#FFFFFF",
+		fontSize: 16,
+	},
+});
