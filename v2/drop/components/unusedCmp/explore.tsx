64bcccac0ce6541ca5466b89cf10424649644aad
Bobie Fordjour McCamble Kofi <fordjourbobiem@gmail.com>
2024-06-07 17:22:50 +0000

    chore: moved components
    moved to and unused folder

--- v2/drop/components/unusedCmp/explore.tsx
+++ v2/drop/components/unusedCmp/explore.tsx
@@ -1,11 +1,11 @@
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { StyleSheet, Image, Platform } from 'react-native';
+import { Image, Platform, StyleSheet } from 'react-native';
 
-import { Collapsible } from '@/components/Collapsible';
-import { ExternalLink } from '@/components/ExternalLink';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { Collapsible } from '@/components/unusedCmp/Collapsible';
+import { ExternalLink } from '@/components/unusedCmp/ExternalLink';
+import ParallaxScrollView from '@/components/unusedCmp/ParallaxScrollView';
+import { ThemedText } from '@/components/unusedCmp/ThemedText';
+import { ThemedView } from '@/components/unusedCmp/ThemedView';
 
 export default function TabTwoScreen() {
   return (
